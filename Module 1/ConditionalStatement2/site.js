e74b6ccfcb31bdd04f3d6f22d2dddc213af8b88e
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-04-15 09:58:27 +0700

    calculator demo

---
+++ Module 1/ConditionalStatement2/site.js
@@ -0,0 +1,17 @@
+/*
+1. getElementById(): thuộc tính Id của element
+2. getElementsByTagName(): tên thẻ (tên element)
+3. getElementsByClassName(): thuộc tính class của element
+4. getElementsByName(): thuộc tính name của element
+4. querySelector(): cú pháp của css, trả về 1 selector duy nhất 
+    - document.querySelector('input[name=age]')
+    - document.querySelector('#tb>tbody>tr:first-child>td:nth-child(2)')
+5. querySelectorAll(): trả về 1 danh sách các selector
+    - document.querySelector('input[name=age]')
+*/ 
+/*
+1. Khai sự kiện trực tiếp tại element
+2. addEventListener
+3. function expression
+ */
+
